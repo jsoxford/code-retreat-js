18dfa03eefbb78e7f7503c9259c37b1d5d83059d
Ryan Brooks <ryanbrooksis@gmail.com>
2014-06-30 08:17:30 +0100

    WIP. Mocha tests on reload. TODO:
    * Mocha only picks up tests on first run (Event Emitter leak)
    * Run function in sandbox

--- bin/setupForJS.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-echo "Make sure to run this script from the project root, i.e."
-echo "  ./bin/setupForJS.sh"
-npm install
--- sessions/1.js
+++ sessions/1.js
@@ -1,18 +1,28 @@
-exports.sessionId = 0;
-exports.team = [
-  {"name": "Ryan Brooks"},
-  {"name": "Ben Foxall"}
-];
+// exports.sessionId = 0;
+// exports.team = [
+//   {"name": "Ryan Brooks"},
+//   {"name": "Ben Foxall"}
+// ];
+//
+// exports.processIteration = function(generation0){
+//   return "00010010011110111101011010101";
+// }
+//
+// exports.runTests = function(){
+//     console.log("Whoop, I ran a test");
+//     return {
+//         testsRun: 110,
+//         testsFailed: 23,
+//         testsIgnored: 1
+//     }
+// }
 
-exports.processIteration = function(generation0){
-  return "000100100111101011101011010101";
-}
-
-exports.runTests = function(){
-    console.log("Whoop, I ran a test");
-    return {
-        testsRun: 110,
-        testsFailed: 23,
-        testsIgnored: 1
-    }
-}
+describe("Something", function () {
+    it("Should do something cool", function () {
+        (5).should.be.exactly(5).and.be.a.Number;
+        //(5).should.be.exactly(3).and.be.a.String;
+    });
+    it("Should do something else cool", function () {
+        (5).should.be.exactly(5).and.be.a.Number;
+    });
+});
