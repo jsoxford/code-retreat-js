b5eee850afc5941155d7185801a25951f024e864
Ryan Brooks <ryanbrooksis@gmail.com>
2014-07-03 21:53:40 +0100

    Added 'cr' command, and got it talking to the server

--- sessions/1.js
+++ sessions/1.js
@@ -1,13 +1,14 @@
 // Session 0, I'm awesome
 
-function tick(generation0){
-
+exports.tick = function(generation0){
+    console.log("TICK!");
+    return generation0;
 }
 
 describe("Something", function () {
     it("Should do something cool", function () {
         (5).should.be.exactly(5).and.be.a.Number;
-        //(5).should.be.exactly(3).and.be.a.String;
+        (5).should.be.exactly(3).and.be.a.String;
     });
     it("Should do something else cool", function () {
         (5).should.be.exactly(5).and.be.a.Number;
