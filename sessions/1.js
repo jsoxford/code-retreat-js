83187cba75a4d3edd1550207a0d624cd9c2cef17
Ryan Brooks <ryanbrooksis@gmail.com>
2014-07-05 20:15:46 +0100

    Updated to use new format for messaging

--- sessions/1.js
+++ sessions/1.js
@@ -1,35 +1,37 @@
 // Session 0, I'm awesome
 
 exports.tickCell = function(generation0){
+  var lives;
     var liveCellsIncludingSelf = generation0.match(/1/g);
     if(generation0[4] === '1'){
 
       //Any live cell with fewer than two live neighbours dies, as if caused by under-population.
       if(liveCellsIncludingSelf.length < 3){
-        return false;
+        lives = false;
       }
 
       //Any live cell with two or three live neighbours lives on to the next generation.
       else if(liveCellsIncludingSelf.length == 3 || liveCellsIncludingSelf.length == 4){
-        return true;
+        lives = true;
       }
 
       //Any live cell with more than three live neighbours dies, as if by overcrowding.
       else{
-        return false;
+        lives = false;
       }
 
     }
     else{
       //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
       if(liveCellsIncludingSelf.length == 3){
-        return true;
+        lives = true;
       }
       else{
-        return false;
+        lives = false;
       }
     }
-    return 'x';
+
+    return !lives;
 }
 
 exports.tickBoard = function(generation0){
